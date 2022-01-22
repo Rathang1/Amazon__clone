bccaa87f79682d6efbc78ee6907077c89cbb9b23
Rathang1 <rathag.rajpal@gmail.com>
2022-01-22 17:32:18 -0600

    completed home page with routes

--- src/Home.js
+++ src/Home.js
@@ -18,39 +18,39 @@ function Home() {
             image ="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg" 
             rating={5} />
             <Product 
-            id = "123456"
-            title = 'The lean startup' 
-            price ={29.99} 
-            image ="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg" 
+            id = "123457"
+            title = 'Newest Playstation 5 Disc Version PS5 Console with One Wireless Controller, 4K-TV Gaming, 16GB GDDR6 RAM, 8K Output, WiFi 6. Ultra-High Speed 825GB SSD - LPT Ultra High Speed HDMI Cable' 
+            price ={960} 
+            image ="https://m.media-amazon.com/images/I/51Qt8XM4V+L._AC_UY327_QL65_.jpg" 
             rating={5} />
          </div>
          <div className='home__row'>
             <Product 
-            id = "123456"
-            title = 'The lean startup' 
-            price ={29.99} 
-            image ="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg" 
+            id = "123458"
+            title = 'Apple iPhone 13 Pro, 128GB, Sierra Blue - Unlocked' 
+            price ={1350.00} 
+            image ="https://m.media-amazon.com/images/I/610xvgzVUDL._AC_UY327_QL65_.jpg" 
             rating={5} />
             <Product 
-            id = "123456"
-            title = 'The lean startup' 
-            price ={29.99} 
-            image ="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg" 
+            id = "123459"
+            title = 'Kindle Oasis – Now with adjustable warm light – Ad-Supported' 
+            price ={250.99} 
+            image ="https://m.media-amazon.com/images/I/61moEqqnwzL._AC_UY327_QL65_.jpg" 
             rating={5} />
             <Product 
-            id = "123456"
-            title = 'The lean startup' 
-            price ={29.99} 
-            image ="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg" 
+            id = "123460"
+            title = 'Lamborghini Sian FKP 37 Red with Copper Wheels 118 Diecast' 
+            price ={100000} 
+            image ="https://m.media-amazon.com/images/I/61ISFQkQ1KL._AC_UL480_QL65_.jpg" 
             rating={5} />
          </div>
          <div className='home__row'>
             <Product 
-            id = "123456"
-            title = 'The lean startup' 
-            price ={29.99} 
-            image ="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg" 
-            rating={5} />
+            id = "123461"
+            title = 'SAMSUNG 34-Inch Odyssey G5 Ultra-Wide Gaming Monitor with 1000R Curved Screen, 165Hz, 1ms, FreeSync Premium, WQHD (LC34G55TWWNXZA, 2020 Model), Black ' 
+            price ={627.14} 
+            image ="https://m.media-amazon.com/images/I/61XDeaOrrKL._AC_UY327_QL65_.jpg" 
+            rating={4} />
              
          </div>
       </div>

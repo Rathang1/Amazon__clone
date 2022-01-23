30f9f130b34788e8a45d5478f60993389d2bb9fd
Rathang1 <rathag.rajpal@gmail.com>
2022-01-22 21:44:47 -0600

    added data layer

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './Header.css'
+import { Link } from 'react-router-dom';
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import { useStateValue } from './StateProvider';
 
 function Header() {
+
+    const [{basket}, dispatch] = useStateValue();
   return (
     <div className='header'>
+        <Link to="/">
         <img className='header__logo' src='https://pngimg.com/uploads/amazon/amazon_PNG11.png'/>
-
+        </Link>
         <div
         className='header__search'>
             <input
@@ -33,10 +38,12 @@ function Header() {
                 <span
                 className='header__optionLineTwo'>Prime</span>                
             </div>
+            <Link to='/checkout'>
             <div className="header_optionBasket">
                 <ShoppingBasketIcon />
-                <span className='header__optionLineTwo header__basketCount'>0</span>
+                <span className='header__optionLineTwo header__basketCount'>{basket?.length}</span>
             </div>
+            </Link>
         </div>
     </div>
   )}

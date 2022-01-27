512d22fdb06f7d8af2bb3d62d6becfb9bbe22f85
Rathang1 <rathag.rajpal@gmail.com>
2022-01-27 00:26:07 -0600

    added login page

---
+++ src/Login.js
@@ -0,0 +1,46 @@
+import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import './Login.css'
+
+function Login() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signIn = e =>{
+        e.preventDefault();
+
+    }
+
+    const register = e =>{
+        e.preventDefault();
+    }
+
+  return (<div className='login'>
+      <Link to="/">
+        <img 
+        className="login__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/800px-Amazon_logo.svg.png" />
+      </Link>
+
+      <div className="login__container">
+          <h1>Sign In</h1>
+          <form>
+              <h5>Email</h5>
+              <input type='text' value={email} onChange={e=> setEmail(e.target.value)} />
+
+              <h5>Password</h5>
+              <input type='password' value={password} onChange={e=> setPassword(e.target.value)} />
+
+              <button className='login__signInButton'
+              type="submit" onClick={signIn}>Sig In</button>
+          </form>
+
+          <p>By continuing, you agree to Amazon's CLONE Conditions of Use and Privacy Notice. </p>
+
+          <button onClick={register} className='login__registerButton'>Create your Account</button>
+      </div>
+  </div>)
+}
+
+export default Login;

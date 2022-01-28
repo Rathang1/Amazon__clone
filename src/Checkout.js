10c800c5001192800d122c5fa47291b6932083aa
Rathang1 <rathag.rajpal@gmail.com>
2022-01-27 23:04:58 -0600

    deployed

--- src/Checkout.js
+++ src/Checkout.js
@@ -6,7 +6,7 @@ import Subtotal from "./Subtotal"
 
 function Checkout() {
 
-    const [{basket}, dispatch] = useStateValue();
+    const [{basket, user}, dispatch] = useStateValue();
   return( 
   <div className='checkout'>
       <div className="checkout__left">
@@ -14,6 +14,7 @@ function Checkout() {
           alt="" />
 
           <div>
+              <h3>hello {user?.email}</h3>
               <h2 className='checkout__title'> Your shopping Basket</h2>
 
               {basket.map(item =>(

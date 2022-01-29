3572e15e0d63fb80dc3a4d09dd5f556b34c69ced
Rathang1 <rathag.rajpal@gmail.com>
2022-01-29 13:54:12 -0600

    added payment functionality

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,36 @@
 const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")('sk_test_51KNLPHHdRNktWjdxAPLrScgctqwHZ1CkRbBLOMOHFyqQE2MiWh1nkufrxFpAbI8qSm48XQZygFtTgOBXsr6YdSyD00pEDqK8ik');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// API stup
+
+// App config
+const app = express();
+
+// Middlewares
+app.use(cors({ origin: true}));
+app.use(express.json());
+
+// API routes
+app.get('/', (request, response) => response.status(200).send('hello Amazon'))
+app.post('/payments/create', async (request, response) => {
+    const total = request.query.total;
+
+    console.log('Payment Req recieved >>', total)
+
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount:total,
+        currency: 'usd',
+
+    });
+
+    response.status(201).send({
+        clientSecret: paymentIntent.client_secret,
+    })
+})
+
+// Listen command
+exports.api = functions.https.onRequest(app)
+
+// http://localhost:5001/challenge-10fc8/us-central1/api
